88bdf24b172152bbe8952f92f5dcab7e39a549f9
Orlando Cofre <ocofre@sistran.com.ec>
2018-07-24 06:51:46 -0500

    Arreglo modelo periodo

--- app/Period.php
+++ app/Period.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Period extends Model
 {
     protected $table ="periods";
-    protected $fillable = ['year','month_id','onth_name'];
+    protected $fillable = ['year','month_id','month_name'];
 
 
     public function payments()
